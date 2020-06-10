43d349134b537f816c4b9ce6926aa63982e8344b
hyoga-yamamoto <20gahyo@gmail.com>
2020-06-10 16:12:52 +0900

    １

--- config/application.rb
+++ config/application.rb
@@ -8,6 +8,9 @@ Bundler.require(*Rails.groups)
 
 module ChatSpace1
   class Application < Rails::Application
+
+    config.time_zone = 'Tokyo'
+
     config.generators do |g|
     config.i18n.default_locale = :ja
       g.stylesheets false
