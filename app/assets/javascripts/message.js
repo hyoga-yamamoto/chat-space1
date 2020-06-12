ba40fad8a9dec8cf7e4c2f07237bfeb2bf2e0494
hyoga-yamamoto <20gahyo@gmail.com>
2020-06-12 16:37:56 +0900

    コミ

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,4 +1,5 @@
 $(function(){ 
+
   function buildHTML(message){
     if ( message.image ) {
       var html =
@@ -39,27 +40,47 @@ $(function(){
      return html;
    };
  }
-$('#new_message').on('submit', function(e){
-  e.preventDefault();
-  var formData = new FormData(this);
-  var url = $(this).attr('action')
+ 
+ var reloadMessages = function() {
+  //カスタムデータ属性を利用し、ブラウザに表示されている最新メッセージのidを取得
+  var last_message_id = $('.message:last').data("message-id");
   $.ajax({
-    url: url,
-    type: "POST",
-    data: formData,
+    //ルーティングで設定した通りのURLを指定
+    url: "api/messages",
+    //ルーティングで設定した通りhttpメソッドをgetに指定
+    type: 'get',
     dataType: 'json',
-    processData: false,
-    contentType: false
+    //dataオプションでリクエストに値を含める
+    data: {id: last_message_id}
   })
-    .done(function(data){
-      var html = buildHTML(data);
-      $('.messages').append(html);
-      $('form')[0].reset();
-      $('.messages').animate({ scrollTop: $('.messages')[0].scrollHeight});
-    })
-    .fail(function() {
-      alert('メッセージを送信できません');
+  .done(function(messages) {
+    if (messages.length !== 0) {
+    //追加するHTMLの入れ物を作る
+    var insertHTML = '';
+    //配列messagesの中身一つ一つを取り出し、HTMLに変換したものを入れ物に足し合わせる
+    $.each(messages, function(i, message) {
+      insertHTML += buildHTML(message)
     });
-    return false;
-})
+    //メッセージが入ったHTMLに、入れ物ごと追加
+    $('.messages').append(insertHTML);
+    $('.messages').animate({ scrollTop: $('.messages')[0].scrollHeight});
+    }
+  })
+  .fail(function() {
+    alert('error');
+  });
+  return false;
+  
+  };
+  setInterval(reloadMessages, 7000);
+
+  if (document.location.href.match(/\/groups\/\d+\/messages/)) {
+    setInterval(reloadMessages, 7000);
+  }
 });
+
+
+// .fail(function() {
+//   alert('メッセージを送信できません');
+// });
+// return false;
